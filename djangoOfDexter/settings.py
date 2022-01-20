a15633d1043f03fa60b50670cf6be3bc1df6e7c9
MinseongLee <dexlee4567@gmail.com>
2022-01-20 18:11:22 +0900

    ORM, 템플릿, 폼 작업

--- djangoOfDexter/settings.py
+++ djangoOfDexter/settings.py
@@ -41,7 +41,7 @@ ROOT_URLCONF = 'djangoOfDexter.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'blog/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
