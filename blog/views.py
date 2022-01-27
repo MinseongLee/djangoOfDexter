d54209c072b771f4fb997da3d462dd14b9fa6590
MinseongLee <dexlee4567@gmail.com>
2022-01-27 12:56:45 +0900

    redis 추가 및 적용

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.core.cache import cache
 from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
@@ -8,7 +9,8 @@ from .models import Comment, Post
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = cache.get_or_set('posts', Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date'))
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
