0f62e8a83d17cadaa7917e73fbe1d7003871428c
MinseongLee <dexlee4567@gmail.com>
2022-02-03 10:44:28 +0900

    url path 통일

--- djangoOfDexter/urls.py
+++ djangoOfDexter/urls.py
@@ -6,6 +6,8 @@ from django.urls import include, path, re_path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    re_path(r'^login/$', LoginView.as_view(), name='login'),
-    re_path(r'^logout/$', LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
+    path('login/', LoginView.as_view(), name='login'),
+    # re_path(r'^login/$', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
+    # re_path(r'^logout/$', LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
 ]
