50ef9d2816bb44153398fca4286470ccb86a108d
MinseongLee <dexlee4567@gmail.com>
2022-02-09 10:15:20 +0900

    redis 커넥션 소켓 세팅

--- djangoOfDexter/settings.py
+++ djangoOfDexter/settings.py
@@ -86,6 +86,10 @@ CACHES = {
         'LOCATION': 'redis://127.0.0.1:6379/1',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+            'SOCKET_TIMEOUT': 5,
+            'SOCKET_CONNECT_TIMEOUT': 5,
+            'max_connections': 50,
+            'timeout': 20,
         }
     }
 }
